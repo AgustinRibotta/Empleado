5315aa593a744310bd3010b738a138b6009216f2
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2023-02-20 20:33:08 -0300

    archivoprod

---
+++ empleado/settings/prod.py
@@ -0,0 +1,27 @@
+from .base import *
+import os
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'empleado',
+        'USER': 'postgres',
+        'PASSWORD': 'empleado1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static') 
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
